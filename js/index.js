3bb77acf8ddfc605607f11ba323564550bfbeb69
Pankaj Kumar Verma <patel.dp2024@gmail.com>
2022-06-14 13:51:23 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var target_date = new Date().getTime() + (1000*3600*48*5); // set the countdown date
+var target_date = new Date('Jun 25 2022').getTime(); // set the countdown date
 var days, hours, minutes, seconds; // variables for time units
 
 var countdown = document.getElementById("tiles"); // get tag element
